d25634cf3941f7217d34b5514d8619a1e4e1378c
Utkarsh Ragghuvanshi <utkarshragghuvanshi@Utkarshs-MacBook-Air.local>
2025-10-27 13:41:56 +0530

    Add More data

--- lib/mockdata.ts
+++ lib/mockdata.ts
@@ -26,4 +26,34 @@ export const mockProducts: Product[] = [
     image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30",
     category: "Electronics",
   },
+  {
+    id: "3",
+    name: "Bagpack",
+    description:
+      "Fitness tracker with heart rate monitoring and sleep analysis.",
+    price: 149.99,
+    image:
+      "https://images.unsplash.com/photo-1575844264771-892081089af5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QmFncGFja3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=900",
+    category: "Electronics",
+  },
+  {
+    id: "4",
+    name: "Shoes",
+    description:
+      "Fitness tracker with heart rate monitoring and sleep analysis.",
+    price: 149.99,
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=900",
+    category: "Electronics",
+  },
+  {
+    id: "5",
+    name: "Coffee Mug",
+    description:
+      "Fitness tracker with heart rate monitoring and sleep analysis.",
+    price: 149.99,
+    image:
+      "https://images.unsplash.com/photo-1514228742587-6b1558fcca3d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8TXVnfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
+    category: "Electronics",
+  },
 ];
