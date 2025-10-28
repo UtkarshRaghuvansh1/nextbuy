74c42309736e120a5f14744650207da145a8d255
Utkarsh Ragghuvanshi <utkarshragghuvanshi@Utkarshs-MacBook-Air.local>
2025-10-28 12:53:08 +0530

    Populating the Category Table

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,31 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+  console.log("Seeding Data...");
+  await prisma.product.deleteMany();
+  console.log("Deleted all rows for  Product table");
+  await prisma.category.deleteMany();
+  console.log("Deleted all rows for  Category table");
+
+  //  Populate table for different categories
+  const electronics = await prisma.category.create({
+    data: {
+      name: "Electronics",
+      slug: "electronics",
+    },
+  });
+  const clothing = await prisma.category.create({
+    data: {
+      name: "Clothing",
+      slug: "clothing",
+    },
+  });
+  const home = await prisma.category.create({
+    data: {
+      name: "Home",
+      slug: "home",
+    },
+  });
 }
 
 main()
